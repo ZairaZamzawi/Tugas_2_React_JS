35c8aad9212507f130bc18348c8daa98c26ef891
Zaira Maulidin Zamzawi <zairazamzawi@gmail.com>
2019-08-11 23:31:36 +0700

    Adding Files
    Tugas 2 React Js

---
+++ src/Page/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class Footer extends Component {
+    render() {
+        return(
+            <div>
+                <p>Tugas 2 React Js</p>
+            </div>
+        );
+    }
+}
+
+export default Footer;
